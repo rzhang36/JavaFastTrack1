ec2c17ebfe9b6c6a3ce3b7110a25ec8328861cc0
rzhang36 <rui-zhang-3@uiowa.edu>
2024-03-03 18:13:27 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/lecture1hm/Lecture1HmApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.lecture1hm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture1HmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture1HmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/lecture1hm/Lecture1HmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.lecture1hm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture1HmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
